dcc2574397d8408515517cedd2034a2d209e0053
Joe Fields <33464023+joedajo@users.noreply.github.com>
2020-11-24 16:02:10 -0500

    Added encryption when message is sent

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
 import socket
 import pickle
+from Crypto.Cipher import AES
 
 HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1235))
 #connect to server
+key = b'Sixteen byte key'
+
 
 while True:
     
     full_msg = b''
     new_msg = True;
     while True:
-        msg = s.recv(16)
+        msg = s.recv(16)    #buffer of 16 bytes at a time
         if new_msg:
             print(f"new message length: {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
@@ -24,8 +27,18 @@ while True:
             print("full msg recvd")
             print(full_msg[HEADERSIZE:])
 
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
+            #receive transmitted nonce once all data comes in
+            ciphertext, tag, nonce = pickle.loads(full_msg[HEADERSIZE:])
+            #create corresponding AES object
+            cipher = AES.new(key, AES.MODE_EAX, nonce = nonce)
+            plaintext = cipher.decrypt(ciphertext)
+
+            try:
+                cipher.verify(tag)
+                print("the message is authentic: ", plaintext)
+            except:
+                print("Key incorrect or message corrupted")
+            
             
             new_msg = True
             full_msg = b''
--- server.py
+++ server.py
@@ -1,9 +1,30 @@
 import socket
 import time
 import pickle
-
+from Crypto.Cipher import AES
 
 HEADERSIZE = 10
+
+#key and AES object
+key = b'Sixteen byte key'
+cipher = AES.new(key, AES.MODE_EAX)
+
+#Encryption--------------------------------------
+#data to be encrypted
+data = b"joe"
+
+#create tuple with ciphertext and MAC
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+#Nonce used in transmission
+nonce = cipher.nonce
+
+#tuple that will be sent to client
+d = ((ciphertext, tag, nonce))
+
+#pack tuple so it can be sent as one to client
+msg = pickle.dumps(d)
+#------------------------------------------------
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #AF_INET corresponds to IPv4
@@ -19,9 +40,6 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept() #clientsocket is another socket object
     print(f"connection from {address} has been esablished!")
-    
-    d = {1: "Hey", 2: "There"}
-    msg = pickle.dumps(d)
 
     msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
     #the < above means left align
